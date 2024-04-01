5568e239dec58b227945e530d919355fd16a1bd4
aacebo <aacebowork@gmail.com>
2024-03-31 21:26:08 -0400

    add global instance

---
+++ global.go
@@ -0,0 +1,27 @@
+package jsonschema
+
+var namespace = New()
+
+func HasFormat(name string) bool {
+	return namespace.HasFormat(name)
+}
+
+func AddFormat(name string, format Formatter) {
+	namespace.AddFormat(name, format)
+}
+
+func AddKeyword(name string, keyword Keyword) {
+	namespace.Keyword(name, keyword)
+}
+
+func Compile(schema Schema) []SchemaError {
+	return namespace.Compile(schema)
+}
+
+func Validate(schema Schema, value any) []SchemaError {
+	return namespace.Validate(schema, value)
+}
+
+func Read(path string) (Schema, error) {
+	return namespace.Read(path)
+}
